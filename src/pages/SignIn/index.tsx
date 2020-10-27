bfe3aa0efbe2f7ae3e07bcbf2fa0e9541cf7fed5
pablo <pablo@radius.net.br>
2020-10-27 17:12:35 -0300

    Adicionando unform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -51,8 +51,20 @@ const SignIn: React.FC = () => {
                         </View>
 
                         <Form ref={formRef} onSubmit={handleSignIn}>
-                            <Input name="email" icon="mail" placeholder="E-mail" />
-                            <Input name="password" icon="lock" placeholder="Senha"/>
+                            <Input
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                keyboardType="email-address"
+                                name="email"
+                                icon="mail"
+                                placeholder="E-mail"
+                            />
+                            <Input
+                                secureTextEntry
+                                name="password"
+                                icon="lock"
+                                placeholder="Senha"
+                            />
 
                             <Button
                                 onPress={() =>{
