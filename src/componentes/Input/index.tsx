37181dc6e8f1edc720fc35e91321cb106f63d064
pablo <pablo@radius.net.br>
2020-10-28 10:41:08 -0300

    melhorias nos erros

--- src/componentes/Input/index.tsx
+++ src/componentes/Input/index.tsx
@@ -64,7 +64,7 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = ({ name, icon,
     }, [fieldName, registerField])
 
     return (
-        <Container isFocused={isFocused}>
+        <Container isFocused={isFocused} isErrored={!!error}>
             <Icon
                 name={icon}
                 size={20}
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,16 +1,20 @@
-import React, {useRef} from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
     Image,
     View,
     ScrollView,
     KeyboardAvoidingView,
     Platform,
-    TextInput
+    TextInput,
+    Alert
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+
+import getValidationErrors from '../../ultils/getValidationErrors';
 
 import Input from '../../componentes/Input';
 import Button from '../../componentes/Button';
@@ -24,6 +28,12 @@ import {
     BackToSignInText
 } from './styles';
 
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
+
 const SignUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
@@ -31,6 +41,39 @@ const SignUp: React.FC = () => {
 
     const emailInputRef = useRef<TextInput>(null);
     const passwordInputRef = useRef<TextInput>(null);
+
+    const handleSignUp = useCallback(async (data: SignUpFormData) => {
+        try {
+            formRef.current?.setErrors({});
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string().required('E-mail obrigatorio').email('Digite um e-mail valido'),
+                password: Yup.string().min(3, 'Minimo de 3 caracteres'),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+
+            // Alert.alert(
+            //     'Cadastro realizado!',
+            //     'Você já pode fazer seu login'
+            // )
+
+        } catch (err) {
+            if (err instanceof Yup.ValidationError){
+                const errors =  getValidationErrors(err);
+
+                formRef.current?.setErrors(errors);
+
+                return;
+            }
+            Alert.alert(
+                'Erro no cadastro',
+                'Ocorreu um erro ao fazer cadastro, tente novamente'
+            );
+        }
+    }, []);
 
     return (
         <>
@@ -48,7 +91,7 @@ const SignUp: React.FC = () => {
                         <View>
                             <Title>Crie sua conta</Title>
                         </View>
-                        <Form ref={formRef} onSubmit={() => {}}>
+                        <Form ref={formRef} onSubmit={handleSignUp}>
 
                             <Input
                                 autoCapitalize="words"
@@ -82,10 +125,12 @@ const SignUp: React.FC = () => {
                                 placeholder="Senha"
                                 textContentType="newPassword"
                                 returnKeyType="send"
-                                onSubmitEditing={() => formRef.current?.submitForm()}
+                                onSubmitEditing={() => {
+                                    formRef.current?.submitForm()
+                                }}
                             />
 
-                            <Button onPress={() =>console.log('oi')}>Entrar</Button>
+                            <Button onPress={() => {formRef.current?.submitForm();}}>Entrar</Button>
                         </Form>
                     </Container>
                 </ScrollView>
